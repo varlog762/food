2f4e10df06d7d23810317468ad94aa5ce365823f
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-08-10 16:46:34 +0200

    feat: add addTabActive function

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,26 @@
 import '../scss/style.scss';
 import {
   hideTabContenet,
-  removeTabActive,
   showTabContent,
+  removeTabActive,
+  addTabActive,
 } from './utils';
 
 document.addEventListener('DOMContentLoaded', () => {
   const tabContainer = document.querySelector('.tabcontainer');
-  const tabHeadersCollection = tabContainer.querySelectorAll('.tabheader__item');
+  const tabHeadersCollection = Array.from(tabContainer.querySelectorAll('.tabheader__item'));
   const tabsContent = tabContainer.querySelectorAll('.tabcontent');
+
+  hideTabContenet(tabsContent);
+  showTabContent(tabsContent, 0);
+  addTabActive(tabHeadersCollection, 0);
 
   document.addEventListener('click', (event) => {
     if (event.target.closest('.tabheader__item')) {
       removeTabActive(tabHeadersCollection);
       event.target.classList.add('tabheader__item_active');
-      // hideTabContenet(tabsContent);
+      hideTabContenet(tabsContent);
+      showTabContent(tabsContent, tabHeadersCollection.indexOf(event.target));
     }
   });
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -4,10 +4,14 @@ export function hideTabContenet(collection) {
   collection.forEach((item) => item.style.display = 'none');
 }
 
-export function showTabContent(collection, idx) {
+export function showTabContent(collection, idx = 0) {
   collection[idx].style.display = 'block';
 }
 
 export function removeTabActive(collection) {
   collection.forEach((item) => item.classList.remove('tabheader__item_active'));
+}
+
+export function addTabActive(collection, idx = 0) {
+  collection[idx].classList.add('tabheader__item_active');
 }
