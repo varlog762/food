1f572022f690dbc6370df91182c7f1fd899227fc
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-08-11 10:33:42 +0200

    feat: add animation to tabs

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,10 +2,12 @@
 /* eslint-disable no-return-assign */
 export function hideTabContenet(collection) {
   collection.forEach((item) => item.classList.remove('tabcontent_active'));
+  collection.forEach((item) => item.classList.remove('tabcontent_animation'));
 }
 
 export function showTabContent(collection, idx = 0) {
   collection[idx].classList.add('tabcontent_active');
+  collection[idx].classList.add('tabcontent_animation');
 }
 
 export function removeTabActive(collection) {
