92c76ed7a9613370529f0b97b88681912ea18f8b
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-08-15 12:16:01 +0200

    feat: add show/hide modal

--- src/js/main.js
+++ src/js/main.js
@@ -8,9 +8,13 @@ import {
 } from './utils';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Tabs elements:
   const tabContainer = document.querySelector('.tabcontainer');
   const tabHeadersCollection = Array.from(tabContainer.querySelectorAll('.tabheader__item'));
   const tabsContent = tabContainer.querySelectorAll('.tabcontent');
+  // Modal elements:
+  const showModalBtns = Array.from(document.querySelectorAll('[data-modal]'));
+  const modal = document.querySelector('.modal');
 
   hideTabContenet(tabsContent);
   showTabContent(tabsContent);
@@ -24,13 +28,18 @@ document.addEventListener('DOMContentLoaded', () => {
       event.target.classList.add('tabheader__item_active');
       hideTabContenet(tabsContent);
       showTabContent(tabsContent, tabHeadersCollection.indexOf(target));
+    } else if (target && showModalBtns.includes(target)) {
+      modal.classList.add('modal_visible');
+    } else if (target && (target === modal || target.closest('.modal__close'))) {
+      modal.classList.remove('modal_visible');
     }
   });
 
   // Timer
-  const now = new Date();
-  const deadLineMS = now.getTime() + 864000000;
-  const deadLine = new Date(deadLineMS);
+  // const now = new Date();
+  // const deadLineMS = now.getTime() + 864000000;
+  // const deadLine = new Date(deadLineMS);
+  const deadLine = new Date('2023-08-31T06:00:00.000Z');
 
   setClock('.timer', deadLine);
 });
