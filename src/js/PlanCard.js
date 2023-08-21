4c6c934fef1bc8cc96ae0014dd714f681de926f5
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-08-21 18:11:02 +0200

    feat: add server.php

--- src/js/PlanCard.js
+++ src/js/PlanCard.js
@@ -15,16 +15,21 @@ export default class PlanCard {
   }
   createCardElement() {
     const cardElement = document.createElement('div');
+
+    if (this.classes.length){
+      this.classes.forEach((item) => cardElement.classList.add(item));
+    } else {
+      cardElement.classList.add('menu__item');
+    }
+
     cardElement.innerHTML = `
-    <div class="menu__item">
-      <img src="img/tabs/${this.coverPath}" alt="${this.altForCover}">
-      <h3 class="menu__item-subtitle">${this.title}</h3>
-      <div class="menu__item-descr">${this.descr}</div>
-      <div class="menu__item-divider"></div>
-      <div class="menu__item-price">
-        <div class="menu__item-cost">Цена:</div>
-        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-      </div>
+    <img src="img/tabs/${this.coverPath}" alt="${this.altForCover}">
+    <h3 class="menu__item-subtitle">${this.title}</h3>
+    <div class="menu__item-descr">${this.descr}</div>
+    <div class="menu__item-divider"></div>
+    <div class="menu__item-price">
+      <div class="menu__item-cost">Цена:</div>
+      <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
     </div>`;
     
     return cardElement;
