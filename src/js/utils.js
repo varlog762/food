77bff00caf3ddf021f8d8ab222f232b399d6cdcf
Gregory Pereverzev <greg.pereverzev@gmail.com>
2023-08-23 10:21:32 +0200

    feat: add json decoding to server.php

--- src/js/utils.js
+++ src/js/utils.js
@@ -83,15 +83,24 @@ export function postData(form, message) {
 
     const request = new XMLHttpRequest();
     request.open('POST', 'server.php');
+    request.setRequestHeader('Content-type', 'application/json');
 
     const formData = new FormData(form);
 
-    request.send(formData);
+    const object = {};
+    formData.forEach((value, key) => object[key] = value);
+    const json = JSON.stringify(object);
+
+    request.send(json);
 
     request.addEventListener('load', () => {
       if (request.status === 200) {
         console.log(request.response);
         statusMessage.textContent = message.success;
+        form.reset();
+        setTimeout(() => {
+          statusMessage.remove();
+        }, 2000);
       } else {
         statusMessage.textContent = message.failure;
       }
